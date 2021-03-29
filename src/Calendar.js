9462bd43c2961b32ae043342101c421fcd2c20af
TanishqSinghAnand <anandtanishqs@gmail.com>
2021-03-30 00:35:35 +0530

    Done with sending Email

--- src/Calendar.js
+++ src/Calendar.js
@@ -3,66 +3,92 @@ import { ScheduleMeeting } from "react-schedule-meeting";
 import TextField from "@material-ui/core/TextField";
 import db from "./firebase";
 import Header from "./Components/Header/Header";
+import DayTimePicker from "@mooncake-dev/react-day-time-picker";
+import emailjs from "emailjs-com";
+function timeSlotValidator(slotTime) {
+  const eveningTime = new Date(
+    slotTime.getFullYear(),
+    slotTime.getMonth(),
+    slotTime.getDate(),
+    18,
+    0,
+    0
+  );
+  const morningTime = new Date(
+    slotTime.getFullYear(),
+    slotTime.getMonth(),
+    slotTime.getDate(),
+    12,
+    0,
+    0
+  );
+  const isValid =
+    slotTime.getTime() < eveningTime.getTime() &&
+    slotTime.getTime() > morningTime;
+  return isValid;
+}
 
-const isEmailValid = (email) => {
-  var emailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-  return !!email && typeof email === "string" && email.match(emailformat);
-};
 function Calendar() {
-  
-  const availableTimeslots = [0, 1, 2, 3, 4, 5].map((id) => {
-    return {
-      id,
-      startTime: new Date(
-        new Date(new Date().setDate(new Date().getDate() + id)).setHours(
-          9,
-          0,
-          0,
-          0
-        )
-      ),
-      endTime: new Date(
-        new Date(new Date().setDate(new Date().getDate() + id)).setHours(
-          17,
-          0,
-          0,
-          0
-        )
-      ),
-    };
-  });
-  isEmailValid("anandtanishqs@gmail.com");
+  function sendEmail(e) {
+    e.preventDefault();
+
+    emailjs
+      .sendForm("gmail", "template_def", e.target, "user_UxxNSe08V2cbFBLvnceoH")
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+  }
   return (
     <div className="selector">
-    {/* <Header /> */}
-      <ScheduleMeeting
-        borderRadius={10}
-        primaryColor="#00000"
-        eventDurationInMinutes={30}
-        availableTimeslots={availableTimeslots}
-        onStartTimeSelect={isEmailValid("anandtanishqs@gmail.com")}
+      {/* <Header /> */}
+      <DayTimePicker
+        timeSlotSizeMinutes={30}
+        timeSlotValidator={timeSlotValidator}
       />
+
+      {/* Fields */}
       <div className="sendInfo">
-        <TextField
-          style={{ marginLeft: 50 }}
-          id="outlined-number"
-          label="Number"
-          type="number"
-          InputLabelProps={{
-            shrink: true,
-          }}
-          variant="outlined"
-        />
-        <TextField
-          style={{ marginLeft: 50 }}
-          id="outlined-number"
-          label="Email"
-          type="text"
-          InputLabelProps={{
-            shrink: true,
-          }}
-          variant="outlined"
-        />
+        <form onSubmit={sendEmail}>
+          <TextField
+            style={{ marginLeft: 50 }}
+            id="outlined-number"
+            label="Name"
+            type="Text"
+            InputLabelProps={{
+              shrink: true,
+            }}
+            variant="outlined"
+            name="name"
+          />
+          <TextField
+            style={{ marginLeft: 50 }}
+            id="outlined-number"
+            label="E Mail ID"
+            type="text"
+            InputLabelProps={{
+              shrink: true,
+            }}
+            variant="outlined"
+            name="email"
+          />
+          <TextField
+            style={{ marginLeft: 50 }}
+            id="outlined-number"
+            label="Number"
+            type="number"
+            InputLabelProps={{
+              shrink: true,
+            }}
+            variant="outlined"
+            name="phone_number"
+          />
+          <button type="submit">Submit</button>
+        </form>
       </div>
     </div>
   );
